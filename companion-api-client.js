8dab6d173b0d016711efd81d6d679ac22c4df061
Chris Gårdenberg <webmaster@itssimple.se>
2018-12-21 16:06:50 +0100

    Frontier Auth working, CompanionApiClient Added CompanionApiClient to have an easy way of fetching data

---
+++ companion-api-client.js
@@ -0,0 +1,34 @@
+"use strict";
+
+class CompanionApiClient {
+	constructor(access_token) {
+		this.AccessToken = access_token;
+	}
+
+	async FetchProfile() {
+		return await this.fetchData("profile");
+	}
+
+	async FetchMarket() {
+		return await this.fetchData("market");
+	}
+
+	async FetchShipyard() {
+		return await this.fetchData("shipyard");
+	}
+
+	async fetchData(endpoint) {
+		let result = await fetch(
+			`https://pts-companion.orerve.net/${endpoint}`,
+			{
+				headers: {
+					Authorization: `Bearer ${this.AccessToken}`
+				}
+			}
+		).then(resp => resp.json());
+
+		return result;
+	}
+}
+
+module.exports = { CompanionApiClient };
--- express.js
+++ express.js
@@ -1,9 +1,113 @@
-const express = require('express');
-const serveStatic = require('serve-static');
+require("dotenv").config();
+var URLSearchParams = require("url").URLSearchParams;
+require("es6-promise").polyfill();
+require("isomorphic-fetch");
+
+const express = require("express");
+const serveStatic = require("serve-static");
+const session = require("express-session");
 const app = express();
+
+const CompanionApiClient = require("./companion-api-client").CompanionApiClient;
+
+app.set("trust proxy", 1);
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		cookie: { secure: undefined },
+		resave: false,
+		saveUninitialized: true
+	})
+);
 
 const port = 3000;
 
-app.use(serveStatic('public', { index: ["index.html"]}));
+app.use(serveStatic("public", { index: ["index.html"] }));
+
+function RandomState() {
+	return rand(20, "-journalreader");
+}
+
+function rand(length, current) {
+	current = current ? current : "";
+	return length
+		? rand(
+				--length,
+				"0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz".charAt(
+					Math.floor(Math.random() * 60)
+				) + current
+		  )
+		: current;
+}
+
+app.get("/frontierAuth", (req, res) => {
+	const randomState = RandomState();
+	console.log(req.baseUrl);
+
+	const redirectUrl = `${req.protocol}://${req.get("host")}/callback`;
+
+	res.redirect(
+		`https://auth.frontierstore.net/auth?state=${randomState}&response_type=code&approval_prompt=auto&redirect_uri=${redirectUrl}&client_id=${
+			process.env.FRONTIER_CLIENT_ID
+		}`
+	);
+});
+
+app.get("/callback", async (req, res, next) => {
+	var code = req.query.code;
+	var state = req.query.state;
+
+	var dataParams = new URLSearchParams();
+
+	dataParams.append("grant_type", "authorization_code");
+	dataParams.append("code", code);
+	dataParams.append("client_id", process.env.FRONTIER_CLIENT_ID);
+	dataParams.append("client_secret", process.env.FRONTIER_CLIENT_SECRET);
+	dataParams.append("state", state);
+	dataParams.append(
+		"redirect_uri",
+		`${req.protocol}://${req.get("host")}/callback`
+	);
+
+	let result = await fetch("https://auth.frontierstore.net/token", {
+		method: "POST",
+		body: dataParams,
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded"
+		}
+	})
+		.then(function(response) {
+			if (response.status == 200) return response.json();
+			res.json(res.json());
+			return;
+		})
+		.then(function(blob) {
+			return blob;
+		});
+
+	if (!!result.message) {
+		res.json(result);
+		return;
+	}
+
+	req.session.frontierToken = result;
+	req.session.save(err => {
+		console.log(err);
+	});
+
+	res.redirect("/");
+});
+
+app.get("/fetchPosition", async (req, res) => {
+	if (!!req.session.frontierToken) {
+		let companionClient = new CompanionApiClient(
+			req.session.frontierToken.access_token
+		);
+		let resp = await companionClient.FetchProfile();
+		res.json(resp);
+		return;
+	}
+	res.json({ message: "Not logged in", error: true });
+});
 
 app.listen(port);
