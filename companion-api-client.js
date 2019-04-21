cddd6f0aacb041b90bdb9a4b3bf455fd4fc0e9e5
Chris Gårdenberg <webmaster@itssimple.se>
2019-04-21 10:16:01 +0200

    feat: Added method to fetch journal from cAPI

--- companion-api-client.js
+++ companion-api-client.js
@@ -2,31 +2,36 @@
 'use strict'
 
 class CompanionApiClient {
-  constructor (accessToken) {
+  constructor(accessToken) {
     this.AccessToken = accessToken
   }
 
-  async fetchProfile () {
+  async fetchProfile() {
     const profile = await this.fetchData('profile')
     return profile
   }
 
-  async fetchMarket () {
+  async fetchMarket() {
     const market = await this.fetchData('market')
     return market
   }
 
-  async fetchShipyard () {
+  async fetchShipyard() {
     const shipyard = await this.fetchData('shipyard')
     return shipyard
   }
 
-  async fetchData (endpoint) {
+  async fetchTodaysJournal() {
+    const journal = await this.fetchData('journal');
+    return journal;
+  }
+
+  async fetchData(endpoint) {
     const result = await fetch(`https://companion.orerve.net/${endpoint}`, {
-      headers: {
-        Authorization: `Bearer ${this.AccessToken}`,
-      },
-    })
+        headers: {
+          Authorization: `Bearer ${this.AccessToken}`,
+        },
+      })
       .then((resp) => resp.json())
       .catch((err) => {
         console.error(err)
