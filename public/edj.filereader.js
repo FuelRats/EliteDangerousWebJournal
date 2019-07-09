720ad1c652a1f0020d28b594b9a5c1d67fd7d173
Chris Gårdenberg <webmaster@itssimple.se>
2019-07-09 10:15:02 +0200

    fix: Fixed problem with journal telling people they don't have enough for synthing o2.

--- public/edj.filereader.js
+++ public/edj.filereader.js
@@ -1,158 +1,165 @@
 /* globals document, window, FileReader, setTimeout, edjLogparser, edjGui, require */
-'use strict';
+"use strict";
 
 const edj = {
-  profileDir: null,
-  selDir: null,
-  lastFile: null,
-  lastLine: 0,
-  currentTail: null,
+	profileDir: null,
+	selDir: null,
+	lastFile: null,
+	lastLine: 0,
+	currentTail: null,
 
-  checkFiles(evt) {
-    edj.selDir = evt.target.files;
-    edj.monitorChanges(edj.selDir);
-  },
+	checkFiles(evt) {
+		edj.selDir = evt.target.files;
+		edj.monitorChanges(edj.selDir);
+	},
 
-  copyFilePath(selector) {
-    const target = document.querySelector(selector);
-    target.contenteditable = true;
+	copyFilePath(selector) {
+		const target = document.querySelector(selector);
+		target.contenteditable = true;
 
-    if (document.body.createTextRange) {
-      const range = document.body.createTextRange();
-      range.moveToElementText(target);
-      range.select();
-      range.execCommand('Copy');
-      range.moveToElementText(null);
-      range.select();
-    } else if (window.getSelection && document.createRange) {
-      const r2 = document.createRange();
-      r2.selectNodeContents(target);
-      const sel = window.getSelection();
-      sel.removeAllRanges();
-      sel.addRange(r2);
-      document.execCommand('copy');
-    }
+		if (document.body.createTextRange) {
+			const range = document.body.createTextRange();
+			range.moveToElementText(target);
+			range.select();
+			range.execCommand("Copy");
+			range.moveToElementText(null);
+			range.select();
+		} else if (window.getSelection && document.createRange) {
+			const r2 = document.createRange();
+			r2.selectNodeContents(target);
+			const sel = window.getSelection();
+			sel.removeAllRanges();
+			sel.addRange(r2);
+			document.execCommand("copy");
+		}
 
-    target.contenteditable = false;
-  },
+		target.contenteditable = false;
+	},
 
-  monitorChanges(_selDir) {
-    if (_selDir === null) {
-      return;
-    }
+	monitorChanges(_selDir) {
+		if (_selDir === null) {
+			return;
+		}
 
-    if (_selDir.toString() === '[object FileList]') {
-      const _files = _selDir;
-      const _fileCount = _files.length;
-      let ix = 0;
+		if (_selDir.toString() === "[object FileList]") {
+			const _files = _selDir;
+			const _fileCount = _files.length;
+			let ix = 0;
 
-      while (ix < _fileCount) {
-        if (_files[ix].name.match(/Journal\.\d+\.\d+\.log/gui)) {
-          if (edj.lastFile === null || _files[ix].lastModified > edj.lastFile.lastModified) {
-            edj.lastFile = _files[ix];
-          }
-        }
+			while (ix < _fileCount) {
+				if (_files[ix].name.match(/Journal\.\d+\.\d+\.log/giu)) {
+					if (
+						edj.lastFile === null ||
+						_files[ix].lastModified > edj.lastFile.lastModified
+					) {
+						edj.lastFile = _files[ix];
+					}
+				}
 
-        ix += 1;
-      }
+				ix += 1;
+			}
 
-      const fr = new FileReader();
+			const fr = new FileReader();
 
-      fr.onload = res => {
-        edj.fileOnLoad(res.target.result);
-      };
+			fr.onload = res => {
+				edj.fileOnLoad(res.target.result);
+			};
 
-      fr.readAsText(edj.lastFile, 'UTF-8');
-      setTimeout(() => {
-        edj.monitorChanges(_selDir);
-      }, 1000);
-    } else {
-      const _files = _selDir;
-      const _fileCount = _files.length;
-      let ix = 0;
+			fr.readAsText(edj.lastFile, "UTF-8");
+			setTimeout(() => {
+				edj.monitorChanges(_selDir);
+			}, 1000);
+		} else {
+			const _files = _selDir;
+			const _fileCount = _files.length;
+			let ix = 0;
 
-      while (ix < _fileCount) {
-        if (_files[ix].match(/Journal\.\d+\.\d+\.log/gui)) {
-          if (edj.lastFile === null || _files[ix] !== edj.lastFile) {
-            edj.lastFile = _files[ix];
-          }
-        }
+			while (ix < _fileCount) {
+				if (_files[ix].match(/Journal\.\d+\.\d+\.log/giu)) {
+					if (edj.lastFile === null || _files[ix] !== edj.lastFile) {
+						edj.lastFile = _files[ix];
+					}
+				}
 
-        ix += 1;
-      }
+				ix += 1;
+			}
 
-      const fs = require('fs');
+			const fs = require("fs");
 
-      fs.readFile(`${edj.profileDir}${edj.lastFile}`, {
-        encoding: 'UTF-8'
-      }, (err, str) => {
-        if (err !== null) {
-          console.log(err);
-        }
+			fs.readFile(
+				`${edj.profileDir}${edj.lastFile}`,
+				{
+					encoding: "UTF-8"
+				},
+				(err, str) => {
+					if (err !== null) {
+						console.log(err);
+					}
 
-        if (typeof str !== 'undefined') {
-          edj.fileOnLoad(str);
-        }
-      });
-      setTimeout(async () => {
-        const tailFiles = await edj.loadLogFiles();
-        edj.monitorChanges(tailFiles);
-      }, 1000);
-    }
-  },
+					if (typeof str !== "undefined") {
+						edj.fileOnLoad(str);
+					}
+				}
+			);
+			setTimeout(async () => {
+				const tailFiles = await edj.loadLogFiles();
+				edj.monitorChanges(tailFiles);
+			}, 1000);
+		}
+	},
 
-  fileOnLoad(fileContent) {
-    const lines = fileContent.split('\n');
-    let lineNumber = edj.lastLine;
+	fileOnLoad(fileContent) {
+		edjdata.hasJournalEntries = true;
+		const lines = fileContent.split("\n");
+		let lineNumber = edj.lastLine;
 
-    while (lineNumber < lines.length) {
-      if (edj.lastLine !== lines[lineNumber]) {
-        edjLogparser.parseLogLine(lines[lineNumber]);
-      }
+		while (lineNumber < lines.length) {
+			if (edj.lastLine !== lines[lineNumber]) {
+				edjLogparser.parseLogLine(lines[lineNumber]);
+			}
 
-      lineNumber += 1;
-    }
+			lineNumber += 1;
+		}
 
-    edj.lastLine = lineNumber;
-    edjGui.updateGui();
-  },
+		edj.lastLine = lineNumber;
+		edjGui.updateGui();
+	},
 
-  isJson(line) {
-    try {
-      JSON.parse(line);
-    } catch (ex) {
-      return false;
-    }
+	isJson(line) {
+		try {
+			JSON.parse(line);
+		} catch (ex) {
+			return false;
+		}
 
-    return true;
-  },
+		return true;
+	},
 
-  loadLogFiles() {
-    return edj.selDir;
-  },
+	loadLogFiles() {
+		return edj.selDir;
+	},
 
-  tailLogFile(fileName) {
-    if (fileName === 'null') {
-      return;
-    }
+	tailLogFile(fileName) {
+		if (fileName === "null") {
+			return;
+		}
 
-    if (fileName === edj.currentTail) {
-      return;
-    }
+		if (fileName === edj.currentTail) {
+			return;
+		}
 
-    const {
-      Tail
-    } = require('tail');
+		const { Tail } = require("tail");
 
-    const logTail = new Tail(fileName);
-    logTail.on('line', line => {
-      edjLogparser.parseLogLine(line);
-    });
-    logTail.on('error', error => {
-      console.error(error);
-    });
-    edj.currentTail = fileName;
-  }
-
+		const logTail = new Tail(fileName);
+		logTail.on("line", line => {
+			edjLogparser.parseLogLine(line);
+		});
+		logTail.on("error", error => {
+			console.error(error);
+		});
+		edj.currentTail = fileName;
+	}
 };
-document.getElementById('logDirectory').addEventListener('change', edj.checkFiles, false);
+document
+	.getElementById("logDirectory")
+	.addEventListener("change", edj.checkFiles, false);
--- public/edj.main.js
+++ public/edj.main.js
@@ -1,149 +1,196 @@
 /* global edjGui */
-'use strict'; // eslint-disable-next-line no-var
+"use strict"; // eslint-disable-next-line no-var
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+function _objectSpread(target) {
+	for (let i = 1; i < arguments.length; i++) {
+		var source = arguments[i] != null ? arguments[i] : {};
+		let ownKeys = Object.keys(source);
+		if (typeof Object.getOwnPropertySymbols === "function") {
+			ownKeys = ownKeys.concat(
+				Object.getOwnPropertySymbols(source).filter(function(sym) {
+					return Object.getOwnPropertyDescriptor(
+						source,
+						sym
+					).enumerable;
+				})
+			);
+		}
+		ownKeys.forEach(function(key) {
+			_defineProperty(target, key, source[key]);
+		});
+	}
+	return target;
+}
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _defineProperty(obj, key, value) {
+	if (key in obj) {
+		Object.defineProperty(obj, key, {
+			value,
+			enumerable: true,
+			configurable: true,
+			writable: true
+		});
+	} else {
+		obj[key] = value;
+	}
+	return obj;
+}
 
-var windowIsActive = true;
-window.addEventListener('blur', function () {
-  windowIsActive = false;
+let windowIsActive = true;
+window.addEventListener("blur", function() {
+	windowIsActive = false;
 });
-window.addEventListener('focus', function () {
-  windowIsActive = true;
+window.addEventListener("focus", function() {
+	windowIsActive = true;
 });
 
 function isWindowActive() {
-  const isHidden = document.visibilityState === 'hidden';
-  return isHidden || windowIsActive;
+	const isHidden = document.visibilityState === "hidden";
+	return isHidden || windowIsActive;
 }
 
 const edjdata = {
-  player: {
-    cmdr: null,
-    rank: {
-      cqc: {
-        rank: 0,
-        progress: 0
-      },
-      combat: {
-        rank: 0,
-        progress: 0
-      },
-      empire: {
-        rank: 0,
-        progress: 0
-      },
-      explore: {
-        rank: 0,
-        progress: 0
-      },
-      federation: {
-        rank: 0,
-        progress: 0
-      },
-      trade: {
-        rank: 0,
-        progress: 0
-      }
-    },
-    pos: {
-      StarSystem: null,
-      Docked: true,
-      Body: null,
-      BodyType: null,
-      StarPos: null,
-      Scoopable: null,
-      Supercruise: null
-    },
-    fuel: {
-      current: null,
-      max: null
-    },
-    materials: {
-      Raw: [],
-      Manufactured: []
-    }
-  },
-  gamemode: null,
-  cansynthesizelifesupport: false,
-  canopyBreached: null,
-  oxygenRemaining: null
+	player: {
+		cmdr: null,
+		rank: {
+			cqc: {
+				rank: 0,
+				progress: 0
+			},
+			combat: {
+				rank: 0,
+				progress: 0
+			},
+			empire: {
+				rank: 0,
+				progress: 0
+			},
+			explore: {
+				rank: 0,
+				progress: 0
+			},
+			federation: {
+				rank: 0,
+				progress: 0
+			},
+			trade: {
+				rank: 0,
+				progress: 0
+			}
+		},
+		pos: {
+			StarSystem: null,
+			Docked: true,
+			Body: null,
+			BodyType: null,
+			StarPos: null,
+			Scoopable: null,
+			Supercruise: null
+		},
+		fuel: {
+			current: null,
+			max: null
+		},
+		materials: {
+			Raw: [],
+			Manufactured: []
+		}
+	},
+	gamemode: null,
+	cansynthesizelifesupport: false,
+	canopyBreached: null,
+	oxygenRemaining: null,
+	hasJournalEntries: false
 };
 let positionInterval = null;
-document.querySelector('.winpathButton').addEventListener('click', () => {
-  edj.copyFilePath('#winpath');
+document.querySelector(".winpathButton").addEventListener("click", () => {
+	edj.copyFilePath("#winpath");
 });
 
 const getPlatform = async function getPlatform() {
-  const result = await fetch(`/getPlatform?_=${new Date().getTime()}`).then(resp => resp.json());
-  return result;
+	const result = await fetch(`/getPlatform?_=${new Date().getTime()}`).then(
+		resp => resp.json()
+	);
+	return result;
 };
 
 const _CAPIUpdateData = function _CAPIUpdateData(result) {
-  if (edjdata.player.cmdr === null) {
-    edjdata.player.cmdr = _objectSpread({
-      Commander: result.commander.name
-    }, result.commander);
-  } // We don't see cargo, so we can't make that prediction.
-  // edjdata.cansynthesizelifesupport = null
+	if (edjdata.player.cmdr === null) {
+		edjdata.player.cmdr = _objectSpread(
+			{
+				Commander: result.commander.name
+			},
+			result.commander
+		);
+	} // We don't see cargo, so we can't make that prediction.
+	// edjdata.cansynthesizelifesupport = null
 
+	edjdata.player.pos.StarSystem = result.lastSystem.name; // Ignoring this for now, since it gives false positives if you travel in the same system after undocking
+	// edjdata.player.pos.Body = result.lastStarport.name
 
-  edjdata.player.pos.StarSystem = result.lastSystem.name; // Ignoring this for now, since it gives false positives if you travel in the same system after undocking
-  // edjdata.player.pos.Body = result.lastStarport.name
-
-  edjdata.canopyBreached = result.ship.cockpitBreached;
-  edjdata.oxygenRemaining = result.ship.oxygenRemaining;
-  edjGui.updateGui();
+	edjdata.canopyBreached = result.ship.cockpitBreached;
+	edjdata.oxygenRemaining = result.ship.oxygenRemaining;
+	edjGui.updateGui();
 };
 
 const getPlayerJournal = async function getPlayerJournal() {
-  if (isWindowActive()) {
-    const result = await fetch(`/fetchJournal?_=${new Date().getTime()}`).then(resp => resp.json());
+	if (isWindowActive()) {
+		const result = await fetch(
+			`/fetchJournal?_=${new Date().getTime()}`
+		).then(resp => resp.json());
 
-    if (Boolean(result.error) && result.error) {
-      return;
-    }
+		if (Boolean(result.error) && result.error) {
+			return;
+		}
 
-    edj.fileOnLoad(result.journal.result);
-  }
+		if (!result.journal.result) {
+			return;
+		}
+
+		edjdata.hasJournalEntries = true;
+
+		edj.fileOnLoad(result.journal.result);
+	}
 };
 
 const getUpdatedPosition = async function getUpdatedPosition() {
-  if (isWindowActive()) {
-    const result = await fetch(`/fetchPosition?_=${new Date().getTime()}`).then(resp => resp.json());
+	if (isWindowActive()) {
+		const result = await fetch(
+			`/fetchPosition?_=${new Date().getTime()}`
+		).then(resp => resp.json());
 
-    if (Boolean(result.error) && result.error) {
-      clearInterval(positionInterval);
-      return;
-    }
+		if (Boolean(result.error) && result.error) {
+			clearInterval(positionInterval);
+			return;
+		}
 
-    _CAPIUpdateData(result);
+		_CAPIUpdateData(result);
 
-    await getPlayerJournal();
-    console.log(new Date());
-  }
+		await getPlayerJournal();
+	}
 };
 
 const positionUpdateInterval = 30000;
 
 const checkIsLoggedIn = async function checkIsLoggedIn() {
-  if (isWindowActive()) {
-    const result = await fetch(`/fetchPosition?_=${new Date().getTime()}`).then(resp => resp.json());
+	if (isWindowActive()) {
+		const result = await fetch(
+			`/fetchPosition?_=${new Date().getTime()}`
+		).then(resp => resp.json());
 
-    if (Boolean(result.error) && result.error) {
-      return;
-    }
+		if (Boolean(result.error) && result.error) {
+			return;
+		}
 
-    edjdata.player.platform = await getPlatform();
+		edjdata.player.platform = await getPlatform();
 
-    _CAPIUpdateData(result);
+		_CAPIUpdateData(result);
 
-    await getPlayerJournal();
-    positionInterval = setInterval(() => {
-      getUpdatedPosition();
-    }, positionUpdateInterval);
-  }
+		await getPlayerJournal();
+		positionInterval = setInterval(() => {
+			getUpdatedPosition();
+		}, positionUpdateInterval);
+	}
 };
 
 checkIsLoggedIn();
